3ef5d9070f644cfd10d8249805286c7a74a6ce28
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-07-20 18:28:40 -0300

    Exercicio 1

---
+++ M01-Fundamentos-do-Desenvolvimento-Web/Bloco04_Introducao_a_JavaScript_e_Logica_de_Programacao/B04D01/script1.js
@@ -0,0 +1,21 @@
+const a = 10;
+const b = 15;
+//Adição
+let adicao = a + b;
+console.log('adição' + ' ' + '=' + adicao);
+
+//Subtração
+let subtracao = a - b;
+console.log('Subtração' + ' ' + '=' + subtracao);
+
+//Multiplicação
+let multiplicacao = a * b;
+console.log('Multiplicação' + ' ' + '=' + multiplicacao);
+
+//Divisão
+let divisao = a / b;
+console.log('Divisão' + ' ' + '=' + divisao);
+
+//Módulo
+let modulo = a % b;
+console.log('Módulo' + ' ' + '=' + modulo)
