6f64105d89db407849ff4a43bce1224c9a7f5f48
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-07-20 15:15:11 -0300

    Fim do exercicio de fixação 1

--- M01-Fundamentos-do-Desenvolvimento-Web/Bloco04_Introducao_a_JavaScript_e_Logica_de_Programacao/B04D01/scripts.js
+++ M01-Fundamentos-do-Desenvolvimento-Web/Bloco04_Introducao_a_JavaScript_e_Logica_de_Programacao/B04D01/scripts.js
@@ -1,2 +1,10 @@
 const myName = "Gabriela";
+const birthCity = "Belo Horizonte";
+let birthYear = 1995;
 console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+birthYear = 2030;
+console.log(birthYear);
+birthCity = "Abaete";
+console.log(birthCity);
