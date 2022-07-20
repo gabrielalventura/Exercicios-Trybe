2129f6ad472c5dbf368fe9bb7980e115dbc3fa42
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-07-20 16:01:38 -0300

    Finalização fixação 4

--- M01-Fundamentos-do-Desenvolvimento-Web/Bloco04_Introducao_a_JavaScript_e_Logica_de_Programacao/B04D01/scripts.js
+++ M01-Fundamentos-do-Desenvolvimento-Web/Bloco04_Introducao_a_JavaScript_e_Logica_de_Programacao/B04D01/scripts.js
@@ -26,3 +26,24 @@ else if (notaCandidato < 80 && notaCandidato >= 60){
 else{
   console.log("Você foi reprovada(o)");
 }
+// fim do exercicio de fixação 3
+// inicio do exercicio de fixação 4
+const currentHour = 7;
+let message = "";
+if(currentHour >= 22){
+  message = "Não deveríamos comer nada, é hora de domir";
+}
+else if(currentHour >= 18 && currentHour < 22){
+  message = "Rango da noite, vamos jantar :D"
+}
+else if(currentHour >= 14 && currentHour < 18){
+  message = "Vamos fazer um bolo pro café da tarde?"
+}
+else if(currentHour >= 11 && currentHour < 14){
+  message = "Hora do almoço!!!"
+}
+else{
+  message = "Hmmm, cheiro de café recém passado"
+}
+console.log(message);
+// fim do exercicio de fixação 4
