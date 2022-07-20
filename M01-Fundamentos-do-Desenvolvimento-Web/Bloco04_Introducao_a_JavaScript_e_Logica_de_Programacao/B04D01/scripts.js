ffa305ad050b5c0e0fe841da65242414d1261a20
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-07-20 15:37:18 -0300

    Finalização fixação 3

--- M01-Fundamentos-do-Desenvolvimento-Web/Bloco04_Introducao_a_JavaScript_e_Logica_de_Programacao/B04D01/scripts.js
+++ M01-Fundamentos-do-Desenvolvimento-Web/Bloco04_Introducao_a_JavaScript_e_Logica_de_Programacao/B04D01/scripts.js
@@ -14,3 +14,15 @@ const area = base * height;
 console.log(area);
 const perimeter = base + height + base + height;
 console.log(perimeter);
+// fim do exercicio de fixação 2
+// inicio do exercicio de fixação 3
+const notaCandidato = 75;
+if (notaCandidato >= 80){
+  console.log("Parabéns, você foi aprovada(o)!");
+}
+else if (notaCandidato < 80 && notaCandidato >= 60){
+  console.log("Você está na nossa lista de espera");
+}
+else{
+  console.log("Você foi reprovada(o)");
+}
