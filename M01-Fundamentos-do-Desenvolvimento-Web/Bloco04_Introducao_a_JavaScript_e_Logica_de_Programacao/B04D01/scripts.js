24252933437862a8b0a443d97d618f1c11b04492
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-07-20 15:21:35 -0300

    Finalização fixação 2

--- M01-Fundamentos-do-Desenvolvimento-Web/Bloco04_Introducao_a_JavaScript_e_Logica_de_Programacao/B04D01/scripts.js
+++ M01-Fundamentos-do-Desenvolvimento-Web/Bloco04_Introducao_a_JavaScript_e_Logica_de_Programacao/B04D01/scripts.js
@@ -6,9 +6,11 @@ console.log(birthCity);
 console.log(birthYear);
 birthYear = 2030;
 console.log(birthYear);
-birthCity = "Abaete";
-console.log(birthCity);
 // fim do exercicio de fixação 1
 // inicio do exercicio de fixação 2
 const base = 5;
 let height = 8;
+const area = base * height;
+console.log(area);
+const perimeter = base + height + base + height;
+console.log(perimeter);
