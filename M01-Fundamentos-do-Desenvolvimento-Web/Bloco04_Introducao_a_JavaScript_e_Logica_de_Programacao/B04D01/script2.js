88a6a474da77399ba53c448bf9a2720f1f32900d
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-07-20 18:32:02 -0300

    Exercicio 2

---
+++ M01-Fundamentos-do-Desenvolvimento-Web/Bloco04_Introducao_a_JavaScript_e_Logica_de_Programacao/B04D01/script2.js
@@ -0,0 +1,8 @@
+// Exercicío 2
+const num1 = 800;
+const num2 = 550;
+if(num1 > num2){
+  console.log("O número 1 é o maior número entre os dois");
+} else {
+  console.log("O número 2 é o maior número entre os dois");
+}
