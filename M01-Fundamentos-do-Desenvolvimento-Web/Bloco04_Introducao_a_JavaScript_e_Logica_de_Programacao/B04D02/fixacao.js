95d4dba073b3578848464b52c470d5ccd0262b27
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-07-21 15:38:38 -0300

    Exercicio de fixação FOR

--- M01-Fundamentos-do-Desenvolvimento-Web/Bloco04_Introducao_a_JavaScript_e_Logica_de_Programacao/B04D02/fixacao.js
+++ M01-Fundamentos-do-Desenvolvimento-Web/Bloco04_Introducao_a_JavaScript_e_Logica_de_Programacao/B04D02/fixacao.js
@@ -12,3 +12,9 @@ console.log(indexOfPortfolio);
 //Exercicío 3
 menu.push('Contato');
 console.log(menu);
+
+//For - Exercicío 1
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for( let index =0; index < groceryList.length; index+= 1){
+  console.log(groceryList[index]);
+}
