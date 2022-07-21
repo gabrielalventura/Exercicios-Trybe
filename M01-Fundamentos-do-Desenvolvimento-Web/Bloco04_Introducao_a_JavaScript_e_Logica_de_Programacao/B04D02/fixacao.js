01084fb460d9f15c18e43803321df57a50cf4b32
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-07-21 15:13:57 -0300

    Fim do inicio da fixação

--- M01-Fundamentos-do-Desenvolvimento-Web/Bloco04_Introducao_a_JavaScript_e_Logica_de_Programacao/B04D02/fixacao.js
+++ M01-Fundamentos-do-Desenvolvimento-Web/Bloco04_Introducao_a_JavaScript_e_Logica_de_Programacao/B04D02/fixacao.js
@@ -3,3 +3,12 @@ let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 
 console.log(menuServices);
+
+// Exercicío 2
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+//Exercicío 3
+menu.push('Contato');
+console.log(menu);
